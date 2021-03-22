920c0d9e3acfbe8a9cd9693b1f7e48ddf541438e
KorwinBieniek <korwin-bieniek@wp.pl>
2021-03-22 20:10:19 +0100

    Adapting multiple interfaces

---
+++ 2.JavaCore/src/com/codegym/task/task19/task1903/Solution.java
@@ -0,0 +1,87 @@
+package com.codegym.task.task19.task1903;
+
+/* 
+Adapting multiple interfaces
+
+*/
+
+import java.util.HashMap;
+import java.util.Map;
+
+/* 
+Adapting multiple interfaces
+
+*/
+
+public class Solution {
+    public static Map<String, String> countries = new HashMap<>();
+
+    static {
+        countries.put("UA", "Ukraine");
+        countries.put("US", "United States");
+        countries.put("FR", "France");
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    public static class IncomeDataAdapter implements Customer, Contact {
+        private IncomeData data;
+
+        public IncomeDataAdapter(IncomeData data) {
+            this.data = data;
+        }
+
+        @Override
+        public String getName() {
+            return data.getContactLastName() + ", " + data.getContactFirstName();
+        }
+
+        @Override
+        public String getPhoneNumber() {
+            return String.format("+%d(%2$s)%3$s-%4$s-%5$s", data.getCountryPhoneCode(),
+                    String.format("%010d", data.getPhoneNumber()).substring(0, 3),
+                    String.format("%010d", data.getPhoneNumber()).substring(3, 6),
+                    String.format("%010d", data.getPhoneNumber()).substring(6, 8),
+                    String.format("%010d", data.getPhoneNumber()).substring(8));
+        }
+
+
+        @Override
+        public String getCompanyName() {
+            return data.getCompany();
+        }
+
+        @Override
+        public String getCountryName() {
+            return countries.get(data.getCountryCode());
+        }
+    }
+
+    public static interface IncomeData {
+        String getCountryCode();        // For example: US
+
+        String getCompany();            // For example: CodeGym Ltd.
+
+        String getContactFirstName();   // For example: John
+
+        String getContactLastName();    // For example: Smith
+
+        int getCountryPhoneCode();      // For example: 1
+
+        int getPhoneNumber();           // For example: 991234567
+    }
+
+    public static interface Customer {
+        String getCompanyName();        // For example: CodeGym Ltd.
+
+        String getCountryName();        // For example: United States
+    }
+
+    public static interface Contact {
+        String getName();               // For example: Smith, John
+
+        String getPhoneNumber();        // For example: +1(099)123-45-67
+    }
+}
