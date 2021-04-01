cd238e1cb2e3e590b0fe2cf4e08ce6409af2edca
KorwinBieniek <korwin-bieniek@wp.pl>
2021-04-01 17:51:00 +0200

    Reader wrapper 2

---
+++ 2.JavaCore/src/com/codegym/task/task19/task1912/Solution.java
@@ -0,0 +1,31 @@
+package com.codegym.task.task19.task1912;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+/* 
+Reader Wrapper 2
+
+*/
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+        PrintStream defaultPrintStream = System.out;
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(byteArrayOutputStream));
+        testString.printSomething();
+        System.setOut(defaultPrintStream);
+
+        String result = byteArrayOutputStream.toString();
+        result = result.replaceAll("te", "??");
+        System.out.println(result);
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("This is text for testing");
+        }
+    }
+}
