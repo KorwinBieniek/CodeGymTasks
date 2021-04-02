665e284a24944e31e2ec21eb2f6d28d97118fc06
KorwinBieniek <korwin-bieniek@wp.pl>
2021-04-02 14:33:23 +0200

    Long words

---
+++ 2.JavaCore/src/com/codegym/task/task19/task1925/Solution.java
@@ -0,0 +1,47 @@
+package com.codegym.task.task19.task1925;
+
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+/* 
+Long words
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String fileName1 = args[0];
+        String fileName2 = args[1];
+        String input;
+
+        ArrayList<String> fileContent = new ArrayList<>();
+
+        try (BufferedReader fileReader = new BufferedReader(new FileReader(fileName1))) {
+            while ((input = fileReader.readLine()) != null)
+                fileContent.add(input);
+        } catch (IOException ignore) {
+            /* NOP */
+        }
+
+        StringBuilder sbLine = new StringBuilder();
+        for (String line : fileContent) {
+            String[] splitedLine = line.split(" ");
+            for (String word : splitedLine) {
+                if (word.length() > 6)
+
+                    sbLine.append(word).append(" ");
+            }
+        }
+
+        String resultLine = sbLine.toString().trim().replaceAll(" ", ",");
+        try (FileWriter fileWriter = new FileWriter(fileName2)) {
+            fileWriter.write(resultLine);
+        } catch (IOException ignore) {
+            /* NOP */
+        }
+    }
+}
