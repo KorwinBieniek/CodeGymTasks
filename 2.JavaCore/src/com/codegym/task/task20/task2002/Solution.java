e3c1d7a3658565d3c894d53b0f88536747ff2b91
KorwinBieniek <korwin-bieniek@wp.pl>
2021-04-04 09:15:18 +0200

    Reading and writing to a file: CodeGym

---
+++ 2.JavaCore/src/com/codegym/task/task20/task2002/Solution.java
@@ -0,0 +1,114 @@
+package com.codegym.task.task20.task2002;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/* 
+Reading and writing to a file: CodeGym
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        // You can find your_file_name.tmp in your TMP directory or adjust outputStream/inputStream according to your file's actual location
+        try {
+            File yourFile = File.createTempFile("your_file_name", null);
+            OutputStream outputStream = new FileOutputStream(yourFile);
+            InputStream inputStream = new FileInputStream(yourFile);
+
+            CodeGym codeGym = new CodeGym();
+            // Initialize users field for the codeGym object here
+            codeGym.save(outputStream);
+            outputStream.flush();
+
+            CodeGym loadedObject = new CodeGym();
+            loadedObject.load(inputStream);
+            // Here check that the codeGym object is equal to the loadedObject object
+            System.out.println(codeGym.equals(loadedObject));
+
+            outputStream.close();
+            inputStream.close();
+
+        } catch (IOException e) {
+            //e.printStackTrace();
+            System.out.println("Oops, something wrong with my file");
+        } catch (Exception e) {
+            //e.printStackTrace();
+            System.out.println("Oops, something wrong with save/load method");
+        }
+    }
+
+    public static class CodeGym {
+        public List<User> users = new ArrayList<>();
+
+        public void save(OutputStream outputStream) throws Exception {
+            // Implement this method
+            PrintWriter printWriter = new PrintWriter(outputStream);
+            if (this.users.size() > 0) {
+                for (User current : this.users) {
+
+                    String sb = current.getFirstName() +
+                            "/" +
+                            current.getLastName() +
+                            "/" +
+                            current.getBirthDate().getTime() +
+                            "/" +
+                            current.isMale() +
+                            "/" +
+                            current.getCountry();
+                    printWriter.println(sb);
+                }
+            }
+            printWriter.close();
+        }
+
+        public void load(InputStream inputStream) throws Exception {
+            // Implement this method
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                User user = new User();
+                String[] lineArray = line.split("/");
+                if (lineArray.length < 5) {
+                    break;
+                }
+                user.setFirstName(lineArray[0]);
+                user.setLastName(lineArray[1]);
+                user.setBirthDate(new Date(Long.parseLong(lineArray[2])));
+                user.setMale(Boolean.parseBoolean(lineArray[3]));
+                switch (lineArray[4]) {
+                    case "UNITED_STATES":
+                        user.setCountry(User.Country.UNITED_STATES);
+                        break;
+                    case "UNITED_KINGDOM":
+                        user.setCountry(User.Country.UNITED_KINGDOM);
+                        break;
+                    default:
+                        user.setCountry(User.Country.OTHER);
+                        break;
+                }
+            this.users.add(user);
+        }
+
+        reader.close();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        CodeGym codeGym = (CodeGym) o;
+
+        return users != null ? users.equals(codeGym.users) : codeGym.users == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        return users != null ? users.hashCode() : 0;
+    }
+}
+}
