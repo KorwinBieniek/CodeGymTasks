4bbf63f8ba81a3d3f4c0d066d7acf66720493519
KorwinBieniek <korwin-bieniek@wp.pl>
2021-04-13 08:15:30 +0200

    Hippodrome game (1-5 parts)

---
+++ 3.JavaMultithreading/src/com/codegym/task/task21/task2113/com/codegym/task/task21/task2113/Hippodrome.java
@@ -0,0 +1,19 @@
+package com.codegym.task.task21.task2113;
+
+import java.util.List;
+
+public class Hippodrome {
+    private List<Horse> horses;
+
+    public Hippodrome(List<Horse> horses) {
+        this.horses = horses;
+    }
+
+    public List<Horse> getHorses() {
+        return horses;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
