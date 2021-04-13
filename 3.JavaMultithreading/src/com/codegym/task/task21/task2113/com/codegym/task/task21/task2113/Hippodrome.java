f206fae3a8afd8529c31c3f237ec878712ab4f4d
KorwinBieniek <korwin-bieniek@wp.pl>
2021-04-13 08:16:01 +0200

    Hippodrome game part 6

--- 3.JavaMultithreading/src/com/codegym/task/task21/task2113/com/codegym/task/task21/task2113/Hippodrome.java
+++ 3.JavaMultithreading/src/com/codegym/task/task21/task2113/com/codegym/task/task21/task2113/Hippodrome.java
@@ -1,8 +1,10 @@
 package com.codegym.task.task21.task2113;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Hippodrome {
+    static com.codegym.task.task21.task2113.Hippodrome game;
     private List<Horse> horses;
 
     public Hippodrome(List<Horse> horses) {
@@ -14,6 +16,9 @@ public class Hippodrome {
     }
 
     public static void main(String[] args) {
-
+        game = new com.codegym.task.task21.task2113.Hippodrome(new ArrayList<>());
+        game.getHorses().add(new Horse("Lucky", 3, 0));
+        game.getHorses().add(new Horse("Slevin", 3, 0));
+        game.getHorses().add(new Horse("Homer", 3, 0));
     }
 }
