07c3d33ebe407acf301799a143e50b6c3ee9f439
KorwinBieniek <korwin-bieniek@wp.pl>
2021-04-16 09:23:58 +0200

    Build a WHERE query

---
+++ 3.JavaMultithreading/src/com/codegym/task/task22/task2208/Solution.java
@@ -0,0 +1,36 @@
+package com.codegym.task.task22.task2208;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/* 
+Build a WHERE query
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Map<String, String> paramsMap = new LinkedHashMap<>();
+        paramsMap.put("name", "Ivanov");
+        paramsMap.put("country", "Ukraine");
+        paramsMap.put("city", "Kiev");
+        paramsMap.put("age", null);
+
+        System.out.println(getQuery(paramsMap));
+    }
+
+    public static String getQuery(Map<String, String> params) {
+        StringBuilder queryBuilder = new StringBuilder();
+        for (String s : params.keySet()) {
+            String value = params.get(s);
+            if (value == null) {
+                continue;
+            }
+            if (queryBuilder.toString().length() != 0) {
+                queryBuilder.append(" and ");
+            }
+            queryBuilder.append(s + " = '" + value + "'");
+        }
+        return queryBuilder.toString();
+    }
+}
