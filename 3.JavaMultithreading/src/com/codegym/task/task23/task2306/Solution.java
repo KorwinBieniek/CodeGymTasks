f2da218dd9f3622e0808efbf49ea43e5eb17fdb9
KorwinBieniek <korwin-bieniek@wp.pl>
2021-04-18 07:30:55 +0200

    Inner 2

---
+++ 3.JavaMultithreading/src/com/codegym/task/task23/task2306/Solution.java
@@ -0,0 +1,46 @@
+package com.codegym.task.task23.task2306;
+
+/* 
+Inner 2
+
+*/
+
+public class Solution {
+    private String country;
+    private String city;
+
+    public Solution(String country, String city) {
+        this.country = country;
+        this.city = city;
+    }
+
+    public SuperUser getTrickyUser(String name) {
+        return new SuperUser(name);
+    }
+
+    public String getUserDescription(String name) {
+         return getTrickyUser(name).getDescription();
+    }
+
+    private class SuperUser {
+        private String name;
+
+        // This method can only be accessed within the Solution class because of the inner class's private modifier
+        public SuperUser(String name) {
+            this.name = name;
+        }
+
+        // This method can only be accessed within the Solution class because of the inner class's private modifier
+        public String getDescription() {
+            return String.format("My name is %s. I'm from %s in %s.", this.name, Solution.this.city, Solution.this.country);
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution("the United States", "Seattle");
+        // Inside the Solution class (and we're currently inside it), the getDescription method can be accessed in two ways
+        System.out.println(solution.getTrickyUser("George").getDescription());
+        // From any other inner class, it can only be accessed like this:
+        System.out.println(solution.getUserDescription("George"));
+    }
+}
