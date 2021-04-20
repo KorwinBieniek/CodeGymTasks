0ee1feee09dee0aa10be09aeab9c0efd58bcacc5
Korwin Bieniek <korwin-bieniek@wp.pl>
2021-04-20 07:00:46 +0200

    Arkanoid (part 4)

--- 3.JavaMultithreading/src/com/codegym/task/task24/task2413/Arkanoid.java
+++ 3.JavaMultithreading/src/com/codegym/task/task24/task2413/Arkanoid.java
@@ -1,12 +1,42 @@
 package com.codegym.task.task24.task2413;
 
+import java.util.List;
+
 public class Arkanoid {
     private int width;
     private int height;
 
+    private Ball ball;
+    private Paddle paddle;
+    private List<Brick> bricks;
+
     public Arkanoid(int width, int height) {
         this.width = width;
         this.height = height;
+    }
+
+    public Ball getBall() {
+        return ball;
+    }
+
+    public void setBall(Ball ball) {
+        this.ball = ball;
+    }
+
+    public Paddle getPaddle() {
+        return paddle;
+    }
+
+    public void setPaddle(Paddle paddle) {
+        this.paddle = paddle;
+    }
+
+    public List<Brick> getBricks() {
+        return bricks;
+    }
+
+    public void setBricks(List<Brick> bricks) {
+        this.bricks = bricks;
     }
 
     public int getWidth() {
