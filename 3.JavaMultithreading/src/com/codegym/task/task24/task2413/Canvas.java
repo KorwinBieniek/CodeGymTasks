76ed7d3f034cf1a04a840f5f94b0927e63f19e74
Korwin Bieniek <korwin-bieniek@wp.pl>
2021-04-20 07:05:48 +0200

    Arkanoid (part 12)

--- 3.JavaMultithreading/src/com/codegym/task/task24/task2413/Canvas.java
+++ 3.JavaMultithreading/src/com/codegym/task/task24/task2413/Canvas.java
@@ -64,6 +64,7 @@ public class Canvas {
     }
 
     void print() {
+
         for (int i = 0; i < height + 2; i++) {
             for (int j = 0; j < width + 2; j++) {
                 System.out.print(" ");
