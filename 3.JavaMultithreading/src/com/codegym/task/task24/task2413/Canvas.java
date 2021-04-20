c6201bffd6a9c8a0a5662d754230e43abe9976da
Korwin Bieniek <korwin-bieniek@wp.pl>
2021-04-20 07:05:01 +0200

    Arkanoid (part 10)

--- 3.JavaMultithreading/src/com/codegym/task/task24/task2413/Canvas.java
+++ 3.JavaMultithreading/src/com/codegym/task/task24/task2413/Canvas.java
@@ -8,7 +8,7 @@ public class Canvas {
     public Canvas(int width, int height) {
         this.width = width;
         this.height = height;
-        this.matrix = new char[height+2][width+2];
+        this.matrix = new char[height + 2][width + 2];
     }
 
     public int getWidth() {
@@ -33,5 +33,33 @@ public class Canvas {
 
     public void setMatrix(char[][] matrix) {
         this.matrix = matrix;
+    }
+
+    /**
+     * Draw the passed figure at the specified coordinates using color c.
+     * If the passed array contains one, then they correspond to char c on the canvas.
+     */
+    void drawMatrix(double x, double y, int[][] matrix, char c) {
+        int height = matrix.length;
+        int width = matrix[0].length;
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (matrix[i][j] == 1)
+                    setPoint(x + j, y + i, c);
+            }
+        }
+    }
+
+    /**
+     * Set one point with coordinates (x,y) to color c on the canvas.
+     */
+    void setPoint(double x, double y, char c) {
+        int x0 = (int) Math.round(x);
+        int y0 = (int) Math.round(y);
+        if (y0 < 0 || y0 >= matrix.length) return;
+        if (x0 < 0 || x0 >= matrix[y0].length) return;
+
+        matrix[y0][x0] = c;
     }
 }
