581046229dc13fa21990061d31ddd7ec0625c45f
Korwin Bieniek <korwin-bieniek@wp.pl>
2021-04-20 07:08:18 +0200

    Arkanoid (part 16)

--- 3.JavaMultithreading/src/com/codegym/task/task24/task2413/Paddle.java
+++ 3.JavaMultithreading/src/com/codegym/task/task24/task2413/Paddle.java
@@ -1,8 +1,23 @@
 package com.codegym.task.task24.task2413;
 
 public class Paddle extends BaseObject {
-    public Paddle(double x, double y, double radius) {
-        super(x, y, radius);
+    // Speed
+    private double speed;
+    // Direction (-1: to the left, +1: to the right)
+    private double direction;
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public double getDirection() {
+        return direction;
+    }
+
+    public Paddle(double x, double y) {
+        super(x, y, 3);
+        speed = 1;
+        direction = 0;
     }
 
     @Override
