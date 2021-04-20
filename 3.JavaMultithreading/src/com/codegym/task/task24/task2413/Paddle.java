6c82f9a23a677c88635e9862a1e4bdc1fec24556
Korwin Bieniek <korwin-bieniek@wp.pl>
2021-04-20 07:03:57 +0200

    Arkanoid (part 8)

--- 3.JavaMultithreading/src/com/codegym/task/task24/task2413/Paddle.java
+++ 3.JavaMultithreading/src/com/codegym/task/task24/task2413/Paddle.java
@@ -4,4 +4,14 @@ public class Paddle extends BaseObject {
     public Paddle(double x, double y, double radius) {
         super(x, y, radius);
     }
+
+    @Override
+    void draw(Canvas canvas) {
+
+    }
+
+    @Override
+    void move() {
+
+    }
 }
