3b1081b49b9ad3b3adbed54b7f197272562af640
Korwin Bieniek <korwin-bieniek@wp.pl>
2021-04-23 06:05:49 +0200

    MVC (part 1)

---
+++ 3.JavaMultithreading/src/com/codegym/task/task36/task3608/model/FakeModel.java
@@ -0,0 +1,24 @@
+package com.codegym.task.task36.task3608.model;
+
+import com.codegym.task.task36.task3608.bean.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FakeModel implements Model {
+
+    private DataModel dataModel = new DataModel();
+
+    @Override
+    public DataModel getDataModel() {
+        return dataModel;
+    }
+
+    @Override
+    public void loadUsers() {
+        List<User> users = new ArrayList<>();
+        users.add(new User("A", 1l, 1));
+        users.add(new User("B", 2l, 1));
+        dataModel.setUsers(users);
+    }
+}
