79233649d73e1bd7b031dfd0dd631d6408f9c580
Korwin Bieniek <korwin-bieniek@wp.pl>
2021-04-26 08:40:24 +0200

    Refactoring (part 8)

--- 3.JavaMultithreading/src/com/codegym/task/task29/task2909/human/StudentDatabase.java
+++ 3.JavaMultithreading/src/com/codegym/task/task29/task2909/human/StudentDatabase.java
@@ -16,23 +16,22 @@ public class StudentDatabase {
         System.out.println("Name: " + student.getName() + " Age: " + student.getAge());
     }
 
-    public static void removeStudent(int index) throws IndexOutOfBoundsException {
-        students.remove(index);
+    public static void removeStudent(int index) {
+        if (index >= 0 && index < students.size()) {
+            students.remove(index);
+        }
     }
 
     public static void findJohnOrAlex() {
-        boolean found = false;
         for (int i = 0; i < students.size(); i++) {
-            if (!found) {
-                if (students.get(i).getName().equals("John")) {
-                    System.out.println("John is in the student database.");
-                    found = true;
-                }
+            if (students.get(i).getName().equals("John")) {
+                System.out.println("John is in the student database.");
+                break;
+            }
 
-                if (students.get(i).getName().equals("Alex")) {
-                    System.out.println("Alex is in the student database.");
-                    found = true;
-                }
+            if (students.get(i).getName().equals("Alex")) {
+                System.out.println("Alex is in the student database.");
+                break;
             }
         }
     }
