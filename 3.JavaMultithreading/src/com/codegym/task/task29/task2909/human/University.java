914afacf8ef699756e2c1443163a63d0a0dddeb4
Korwin Bieniek <korwin-bieniek@wp.pl>
2021-04-26 08:29:02 +0200

    Refactoring (part 1)

---
+++ 3.JavaMultithreading/src/com/codegym/task/task29/task2909/human/University.java
@@ -0,0 +1,22 @@
+package com.codegym.task.task29.task2909.human;
+
+public class University extends Student {
+
+    public University(String name, int age) {
+        super(name, age, 0);
+    }
+
+    public Student getStudentWithAverageGrade() {
+        // TODO:
+        return null;
+    }
+
+    public Student getStudentWithHighestAverageGrade(double averageGrade) {
+        // TODO:
+        return null;
+    }
+
+    public void getStudentWithLowestAverageGradeAndExpel() {
+        // TODO:
+    }
+}
