211a4bb4b9948dbb460248082a9eea0640e96bbd
Korwin <korwin-bieniek@wp.pl>
2021-04-30 20:08:04 +0200

    Chat (part 8)

--- 3.JavaMultithreading/src/com/codegym/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/codegym/task/task30/task3008/Server.java
@@ -36,6 +36,34 @@ public class Server {
         public void run() {
 
         }
+
+        private String serverHandshake(Connection connection) throws IOException, ClassNotFoundException {
+            while (true) {
+                connection.send(new Message(MessageType.NAME_REQUEST));
+
+                Message message = connection.receive();
+                if (message.getType() != MessageType.USER_NAME) {
+                    ConsoleHelper.writeMessage("Message received from " + socket.getRemoteSocketAddress() + ". The message type does not match the protocol.");
+                    continue;
+                }
+
+                String userName = message.getData();
+
+                if (userName.isEmpty()) {
+                    ConsoleHelper.writeMessage("There was an attempt to connect to the server using an empty name from " + socket.getRemoteSocketAddress());
+                    continue;
+                }
+
+                if (connectionMap.containsKey(userName)) {
+                    ConsoleHelper.writeMessage("There was an attempt to connect to the server using a name that is already being used from " + socket.getRemoteSocketAddress());
+                    continue;
+                }
+                connectionMap.put(userName, connection);
+
+                connection.send(new Message(MessageType.NAME_ACCEPTED));
+                return userName;
+            }
+        }
     }
 
     public static void sendBroadcastMessage(Message message) {
