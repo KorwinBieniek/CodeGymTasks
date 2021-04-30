f5fadfb34462d4c8ede052191336462092755ea0
Korwin <korwin-bieniek@wp.pl>
2021-04-30 20:10:56 +0200

    Chat (part 12)

---
+++ 3.JavaMultithreading/src/com/codegym/task/task30/task3008/client/Client.java
@@ -0,0 +1,11 @@
+package com.codegym.task.task30.task3008.client;
+
+import com.codegym.task.task30.task3008.Connection;
+
+public class Client {
+    protected Connection connection;
+    private volatile boolean clientConnected;
+
+    public class SocketThread extends Thread {
+    }
+}
