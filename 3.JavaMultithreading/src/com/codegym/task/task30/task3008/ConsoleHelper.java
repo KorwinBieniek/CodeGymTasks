02eb62780b08bb552391fc2fd0d938b60876c33a
Korwin <korwin-bieniek@wp.pl>
2021-04-30 19:57:35 +0200

    Chat (part 2)

--- 3.JavaMultithreading/src/com/codegym/task/task30/task3008/ConsoleHelper.java
+++ 3.JavaMultithreading/src/com/codegym/task/task30/task3008/ConsoleHelper.java
@@ -1,4 +1,35 @@
 package com.codegym.task.task30.task3008;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ConsoleHelper {
+    private static BufferedReader bis = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static String readString() {
+        while (true) {
+            try {
+                String buf = bis.readLine();
+                if (buf != null)
+                    return buf;
+            } catch (IOException e) {
+                writeMessage("An error occurred while trying to enter text. Try again.");
+            }
+        }
+    }
+
+    public static int readInt() {
+        while (true) {
+            try {
+                return Integer.parseInt(readString().trim());
+            } catch (NumberFormatException e) {
+                writeMessage("An error while trying to enter a number. Try again.");
+            }
+        }
+    }
 }
