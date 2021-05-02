68053cfe5be01f6f18d6914441d7ec1e0de36fe7
Korwin <korwin-bieniek@wp.pl>
2021-05-02 11:26:11 +0200

    Archiver (part 17)

--- 3.JavaMultithreading/src/com/codegym/task/task31/task3110/ZipFileManager.java
+++ 3.JavaMultithreading/src/com/codegym/task/task31/task3110/ZipFileManager.java
@@ -129,6 +129,58 @@ public class ZipFileManager {
         Files.move(tempZipFile, zipFile, StandardCopyOption.REPLACE_EXISTING);
     }
 
+    public void addFile(Path absolutePath) throws Exception {
+        addFiles(Collections.singletonList(absolutePath));
+    }
+
+    public void addFiles(List<Path> absolutePathList) throws Exception {
+        // Check whether the zip file exists
+        if (!Files.isRegularFile(zipFile)) {
+            throw new NoSuchZipFileException();
+        }
+
+        // Create a temporary file
+        Path tempZipFile = Files.createTempFile(null, null);
+        List<Path> archiveFiles = new ArrayList<>();
+
+        try (ZipOutputStream zipOutputStream = new ZipOutputStream(Files.newOutputStream(tempZipFile))) {
+            try (ZipInputStream zipInputStream = new ZipInputStream(Files.newInputStream(zipFile))) {
+
+                ZipEntry zipEntry = zipInputStream.getNextEntry();
+                while (zipEntry != null) {
+                    String fileName = zipEntry.getName();
+                    archiveFiles.add(Paths.get(fileName));
+
+                    zipOutputStream.putNextEntry(new ZipEntry(fileName));
+                    copyData(zipInputStream, zipOutputStream);
+
+                    zipInputStream.closeEntry();
+                    zipOutputStream.closeEntry();
+
+                    zipEntry = zipInputStream.getNextEntry();
+                }
+            }
+
+            // Archive new files
+            for (Path file : absolutePathList) {
+                if (Files.isRegularFile(file))
+                {
+                    if (archiveFiles.contains(file.getFileName()))
+                        ConsoleHelper.writeMessage(String.format("File '%s' already exists in the archive.", file.toString()));
+                    else {
+                        addNewZipEntry(zipOutputStream, file.getParent(), file.getFileName());
+                        ConsoleHelper.writeMessage(String.format("File '%s' was added to the archive.", file.toString()));
+                    }
+                }
+                else
+                    throw new PathNotFoundException();
+            }
+        }
+
+        // Move the temporary file to the location of the original
+        Files.move(tempZipFile, zipFile, StandardCopyOption.REPLACE_EXISTING);
+    }
+
     public List<FileProperties> getFileList() throws Exception {
         // Check whether the zip file exists
         if (!Files.isRegularFile(zipFile)) {
@@ -141,7 +193,7 @@ public class ZipFileManager {
             ZipEntry zipEntry = zipInputStream.getNextEntry();
 
             while (zipEntry != null) {
-                // The "size" and "compressed size" fields are unknown until the entry is read
+                // The "size" and "compressed size" fields " are unknown until the entry is read
                 // Let's read it into an output stream
                 ByteArrayOutputStream baos = new ByteArrayOutputStream();
                 copyData(zipInputStream, baos);
