7300ea1d74ec06881a5d5ffb9434ce97eeba8a62
Korwin <korwin-bieniek@wp.pl>
2021-05-02 11:08:11 +0200

    Archiver (part 1)

---
+++ 3.JavaMultithreading/src/com/codegym/task/task31/task3110/Archiver.java
@@ -0,0 +1,18 @@
+package com.codegym.task.task31.task3110;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.file.Paths;
+
+public class Archiver {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader bis = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Enter the full path to the archive file:");
+        ZipFileManager zipFileManager = new ZipFileManager(Paths.get(bis.readLine()));
+
+        System.out.println("Enter the full file name for archiving:");
+        zipFileManager.createZip(Paths.get(bis.readLine()));
+    }
+}
