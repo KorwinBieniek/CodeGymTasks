91ce053f957eac34503837260ba9cdad8a6f3bd9
Korwin <korwin-bieniek@wp.pl>
2021-05-03 05:50:14 +0200

    ReentrantReadWriteLock

---
+++ 3.JavaMultithreading/src/com/codegym/task/task28/task2811/Solution.java
@@ -0,0 +1,14 @@
+package com.codegym.task.task28.task2811;
+
+import java.util.LinkedHashMap;
+
+/* 
+ReentrantReadWriteLock
+
+*/
+
+public class Solution {
+    public static void main(String[] args) throws InterruptedException {
+        ReadWriteMap<Integer, String> linkedSafeMap = new ReadWriteMap<>(new LinkedHashMap<>());
+    }
+}
