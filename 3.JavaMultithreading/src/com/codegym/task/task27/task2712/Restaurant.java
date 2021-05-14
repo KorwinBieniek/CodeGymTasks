fe20e0a6a23620f3ceba6240968d90a23c4c6106
Korwin <korwin-bieniek@wp.pl>
2021-05-14 07:46:08 +0200

    Restaurant (Part 2)

--- 3.JavaMultithreading/src/com/codegym/task/task27/task2712/Restaurant.java
+++ 3.JavaMultithreading/src/com/codegym/task/task27/task2712/Restaurant.java
@@ -2,6 +2,10 @@ package com.codegym.task.task27.task2712;
 
 public class Restaurant {
     public static void main(String[] args) {
-
+        Tablet tablet = new Tablet(5);
+        tablet.createOrder();
+        tablet.createOrder();
+        tablet.createOrder();
+        tablet.createOrder();
     }
 }
--- 3.JavaMultithreading/src/com/codegym/task/task27/task2712/Tablet.java
+++ 3.JavaMultithreading/src/com/codegym/task/task27/task2712/Tablet.java
@@ -1,13 +1,31 @@
 package com.codegym.task.task27.task2712;
 
+import com.codegym.task.task27.task2712.kitchen.Order;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Tablet {
     private final int number;
+    private static Logger logger = Logger.getLogger(Tablet.class.getName());
 
     public Tablet(int number) {
         this.number = number;
     }
 
     public void createOrder() {
+        Order order = null;
+        try {
+            order = new Order(this);
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "The console is unavailable.");
+        }
+    }
 
+    public String toString() {
+        return "Tablet{" +
+                "number=" + number +
+                '}';
     }
 }
