7f53cfd257cf1e71ab1b26af19b00c081da7e1c4
Korwin <korwin-bieniek@wp.pl>
2021-05-15 08:29:42 +0200

    Restaurant (Part 5)

--- 3.JavaMultithreading/src/com/codegym/task/task27/task2712/kitchen/Dish.java
+++ 3.JavaMultithreading/src/com/codegym/task/task27/task2712/kitchen/Dish.java
@@ -1,11 +1,21 @@
 package com.codegym.task.task27.task2712.kitchen;
 
 public enum Dish {
-    Fish,
-    Steak,
-    Soup,
-    Juice,
-    Water;
+    Fish(25),
+    Steak(30),
+    Soup(15),
+    Juice(5),
+    Water(3);
+
+    private int duration;
+
+    public int getDuration() {
+        return duration;
+    }
+
+    private Dish(int duration) {
+        this.duration = duration;
+    }
 
     public static String allDishesToString() {
         String result = "";
