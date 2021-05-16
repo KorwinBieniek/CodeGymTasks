724b749055617f0dc09b93ceb430013f9e850a0e
Korwin <korwin-bieniek@wp.pl>
2021-05-16 11:37:28 +0200

    Restaurant (Part 13)

--- 3.JavaMultithreading/src/com/codegym/task/task27/task2712/statistics/StatisticsManager.java
+++ 3.JavaMultithreading/src/com/codegym/task/task27/task2712/statistics/StatisticsManager.java
@@ -1,19 +1,35 @@
 package com.codegym.task.task27.task2712.statistics;
 
 import com.codegym.task.task27.task2712.statistics.event.EventDataRow;
+import com.codegym.task.task27.task2712.statistics.event.EventType;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class StatisticsManager {
-    public void record(EventDataRow data) {
+    private static StatisticsManager ourInstance = new StatisticsManager();
+
+    public static StatisticsManager getInstance() {
+        return ourInstance;
     }
+
+    private StatisticsStorage statisticsStorage = new StatisticsStorage();
 
     private StatisticsManager() {
     }
 
-    public static StatisticsManager getInstance() {
-        return InstanceHolder.statisticsManager;
+    public void record(EventDataRow data) {
     }
 
-    private static class InstanceHolder {
-        private static StatisticsManager statisticsManager = new StatisticsManager();
+    private class StatisticsStorage {
+        private Map<EventType, List<EventDataRow>> storage = new HashMap<>();
+
+        private StatisticsStorage() {
+            for (EventType type : EventType.values()) {
+                this.storage.put(type, new ArrayList<EventDataRow>());
+            }
+        }
     }
 }
