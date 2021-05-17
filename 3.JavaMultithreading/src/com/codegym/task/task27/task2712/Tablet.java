82d0454389fe1a4ff1e6e5414712ffd7ec714e0d
Korwin <korwin-bieniek@wp.pl>
2021-05-17 11:26:09 +0200

    Restaurant (Part 18)

--- 3.JavaMultithreading/src/com/codegym/task/task27/task2712/Tablet.java
+++ 3.JavaMultithreading/src/com/codegym/task/task27/task2712/Tablet.java
@@ -3,42 +3,70 @@ package com.codegym.task.task27.task2712;
 import com.codegym.task.task27.task2712.ad.AdvertisementManager;
 import com.codegym.task.task27.task2712.ad.NoVideoAvailableException;
 import com.codegym.task.task27.task2712.kitchen.Order;
+import com.codegym.task.task27.task2712.kitchen.TestOrder;
 
 import java.io.IOException;
 import java.util.Observable;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Tablet extends Observable {
     private final int number;
     private static Logger logger = Logger.getLogger(Tablet.class.getName());
+    private LinkedBlockingQueue<Order> queue;
+
 
     public Tablet(int number) {
         this.number = number;
     }
 
-    public Order createOrder() {
+    public void createOrder() {
         Order order = null;
         try {
             order = new Order(this);
-            if (order.isEmpty()) {
-                return null;
-            }
-            AdvertisementManager advertisementManager = new AdvertisementManager(order.getTotalCookingTime() * 60);
-            advertisementManager.processVideos();
-            setChanged();
-            notifyObservers(order);
+            processOrder(order);
         } catch (IOException e) {
             logger.log(Level.SEVERE, "The console is unavailable.");
-        } catch (NoVideoAvailableException nve) {
+        } catch (NoVideoAvailableException e) {
             logger.log(Level.INFO, "No video is available for the following order: " + order);
         }
-        return order;
     }
 
+    private boolean processOrder(Order order) {
+        ConsoleHelper.writeMessage(order.toString());
+        if (order.isEmpty())
+            return true;
+
+//        queue.offer(order);
+
+        setChanged();
+        notifyObservers(order);
+
+        new AdvertisementManager(order.getTotalCookingTime() * 60).processVideos();
+        return false;
+    }
+
+    public void createTestOrder() {
+        Order order = null;
+        try {
+            order = new TestOrder(this);
+            processOrder(order);
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "The console is unavailable.");
+        } catch (NoVideoAvailableException e) {
+            logger.log(Level.INFO, "No video is available for the following order: " + order);
+        }
+    }
+
+    @Override
     public String toString() {
         return "Tablet{" +
                 "number=" + number +
                 '}';
+    }
+
+    public void setQueue(LinkedBlockingQueue<Order> queue) {
+        this.queue = queue;
     }
 }
