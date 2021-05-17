25c80876c11dc3ca9b14ef3084a9014328dbb836
Korwin <korwin-bieniek@wp.pl>
2021-05-17 11:26:40 +0200

    Restaurant (Part 19)

--- 3.JavaMultithreading/src/com/codegym/task/task27/task2712/Restaurant.java
+++ 3.JavaMultithreading/src/com/codegym/task/task27/task2712/Restaurant.java
@@ -1,17 +1,42 @@
 package com.codegym.task.task27.task2712;
 
 import com.codegym.task.task27.task2712.kitchen.Cook;
-import com.codegym.task.task27.task2712.kitchen.Waiter;
+import com.codegym.task.task27.task2712.statistics.StatisticsManager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Restaurant {
     private static final int ORDER_CREATION_INTERVAL = 100;
 
     public static void main(String[] args) {
-        Tablet tablet = new Tablet(5);
-        Cook cook = new Cook("Amigo");
-        Waiter waiter = new Waiter();
-        tablet.addObserver(cook);
-        cook.addObserver(waiter);
+        List<Tablet> tablets = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            tablets.add(new Tablet(i + 1));
+        }
+
+        Cook cook1 = new Cook("John Arne Riise");
+        Cook cook2 = new Cook("Milan Baros");
+
+        StatisticsManager.getInstance().register(cook1);
+        StatisticsManager.getInstance().register(cook2);
+
+        for (Tablet tablet :
+                tablets) {
+            tablet.addObserver(cook1);
+            tablet.addObserver(cook2);
+        }
+
+        Thread thread = new Thread(new RandomOrderGeneratorTask(tablets, ORDER_CREATION_INTERVAL));
+        thread.start();
+
+        try {
+            Thread.sleep(1000);
+            thread.interrupt();
+            thread.join();
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+        }
 
         ManagerTablet managerTablet = new ManagerTablet();
         managerTablet.printAdRevenue();
