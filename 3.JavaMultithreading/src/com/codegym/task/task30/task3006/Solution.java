1ead03131f5991069dc08d3a1e6eab11a849325a
Korwin <korwin-bieniek@wp.pl>
2021-05-20 06:44:06 +0200

    A new way to swap

---
+++ 3.JavaMultithreading/src/com/codegym/task/task30/task3006/Solution.java
@@ -0,0 +1,15 @@
+package com.codegym.task.task30.task3006;
+
+/* 
+A new way to swap
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Pair pair = new Pair(4, 5);
+        System.out.println(pair);
+        pair.swap();
+        System.out.println(pair);
+    }
+}
