66e9fe9d66f7a37f7045817ef05a7561a7779cf8
Korwin <korwin-bieniek@wp.pl>
2021-05-22 07:28:27 +0200

    What's in the folder?

---
+++ 4.JavaCollections/src/com/codegym/task/task31/task3113/Solution.java
@@ -0,0 +1,51 @@
+package com.codegym.task.task31.task3113;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+
+/* 
+What's in the folder?
+
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String dirName = reader.readLine();
+
+        Path directory = Paths.get(dirName);
+
+        if (!Files.isDirectory(directory)) {
+            System.out.println(directory.toString() + " is not a folder");
+        } else {
+            AtomicInteger folderCounter = new AtomicInteger();
+            AtomicInteger fileCounter = new AtomicInteger();
+            AtomicLong sizeCounter = new AtomicLong();
+
+            Files.walkFileTree(directory, new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                    if (!dir.equals(directory)) folderCounter.incrementAndGet();
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    fileCounter.incrementAndGet();
+                    sizeCounter.addAndGet(attrs.size());
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+
+            System.out.println("Total folders: " + folderCounter.get());
+            System.out.println("Total files: " + fileCounter.get());
+            System.out.println("Total size: " + sizeCounter.get() + " bytes");
+        }
+    }
+}
