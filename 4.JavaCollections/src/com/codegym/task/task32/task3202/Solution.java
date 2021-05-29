1cef99bfad686ef257c2e5ed183ba738c3ec7a7b
Korwin <korwin-bieniek@wp.pl>
2021-05-29 09:02:03 +0200

    Reading from a stream

---
+++ 4.JavaCollections/src/com/codegym/task/task32/task3202/Solution.java
@@ -0,0 +1,29 @@
+package com.codegym.task.task32.task3202;
+
+import java.io.*;
+
+/* 
+Reading from a stream
+
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        StringWriter writer = getAllDataFromInputStream(new FileInputStream("testFile.log"));
+        System.out.println(writer.toString());
+    }
+
+    public static StringWriter getAllDataFromInputStream(InputStream is) throws IOException {
+        StringWriter writer = new StringWriter();
+        if (is != null) {
+            char[] buffer = new char[1024];
+            try (Reader reader = new BufferedReader(new InputStreamReader(is))) {
+                int n;
+                while ((n = reader.read(buffer)) != -1) {
+                    writer.write(buffer, 0, n);
+                }
+            }
+        }
+        return writer;
+    }
+}
