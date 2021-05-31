7b9f0754c46a85d9403bc96d707fa45d2ad422c6
Korwin <korwin-bieniek@wp.pl>
2021-05-31 08:06:38 +0200

    Tetris (part 2)

--- 4.JavaCollections/src/com/codegym/task/task22/task2213/Tetris.java
+++ 4.JavaCollections/src/com/codegym/task/task22/task2213/Tetris.java
@@ -1,6 +1,9 @@
 package com.codegym.task.task22.task2213;
 
 public class Tetris {
+    private Field field;
+    private GamePiece gamePiece;
+
     public static void main(String[] args) {
 
     }
