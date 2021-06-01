34fa59b54e59148418067b3d0ec716631a464459
Korwin <korwin-bieniek@wp.pl>
2021-06-01 09:14:20 +0200

    Tetris (part 8)

--- 4.JavaCollections/src/com/codegym/task/task22/task2213/GamePiece.java
+++ 4.JavaCollections/src/com/codegym/task/task22/task2213/GamePiece.java
@@ -1,4 +1,25 @@
 package com.codegym.task.task22.task2213;
 
 public class GamePiece {
+    private int x;
+    private int y;
+    private int[][] matrix;
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int[][] getMatrix() {
+        return matrix;
+    }
+
+    public GamePiece(int x, int y, int[][] matrix) {
+        this.x = x;
+        this.y = y;
+        this.matrix = matrix;
+    }
 }
