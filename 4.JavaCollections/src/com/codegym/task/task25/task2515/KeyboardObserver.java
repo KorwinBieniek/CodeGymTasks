5e8215ee304b516535169727e5df2a7395c88886
Korwin <korwin-bieniek@wp.pl>
2021-06-05 07:09:26 +0200

    Space (part 8)

--- 4.JavaCollections/src/com/codegym/task/task25/task2515/KeyboardObserver.java
+++ 4.JavaCollections/src/com/codegym/task/task25/task2515/KeyboardObserver.java
@@ -2,13 +2,12 @@ package com.codegym.task.task25.task2515;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.Queue;
 import java.util.concurrent.ArrayBlockingQueue;
-
-import java.awt.event.FocusEvent;
-import java.awt.event.FocusListener;
 
 public class KeyboardObserver extends Thread {
     private Queue<KeyEvent> keyEvents = new ArrayBlockingQueue<KeyEvent>(100);
