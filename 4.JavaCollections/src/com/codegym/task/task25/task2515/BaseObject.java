87dce43f23dc2e25c95a89dbd3f4ae5cccc0fa4d
Korwin <korwin-bieniek@wp.pl>
2021-06-05 07:06:56 +0200

    Space (part 6)

--- 4.JavaCollections/src/com/codegym/task/task25/task2515/BaseObject.java
+++ 4.JavaCollections/src/com/codegym/task/task25/task2515/BaseObject.java
@@ -15,6 +15,24 @@ public abstract class BaseObject {
         this.isAlive = true;
     }
 
+    public void draw() {
+    }
+
+    public void move() {
+    }
+
+    public void die() {
+        isAlive = false;
+    }
+
+    public boolean intersects(BaseObject o) {
+        double dx = x - o.x;
+        double dy = y - o.y;
+        double destination = Math.sqrt(dx * dx + dy * dy);
+        double destination2 = Math.max(radius, o.radius);
+        return destination <= destination2;
+    }
+
     public boolean isAlive() {
         return isAlive;
     }
