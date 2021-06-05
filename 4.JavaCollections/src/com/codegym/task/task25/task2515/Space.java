6972ef43549ac56b88c45dac919ddc71be335df1
Korwin <korwin-bieniek@wp.pl>
2021-06-05 07:02:46 +0200

    Space (part 1)

---
+++ 4.JavaCollections/src/com/codegym/task/task25/task2515/Space.java
@@ -0,0 +1,7 @@
+package com.codegym.task.task25.task2515;
+
+public class Space {
+
+    public static void main(String... args) {
+    }
+}
---
+++ 4.JavaCollections/src/com/codegym/task/task25/task2515/Ufo.java
@@ -0,0 +1,4 @@
+package com.codegym.task.task25.task2515;
+
+public class Ufo {
+}
