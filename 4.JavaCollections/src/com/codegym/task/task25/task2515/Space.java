734635068d30ae6662f8ff36b6302e748f684ed4
Korwin <korwin-bieniek@wp.pl>
2021-06-05 07:05:23 +0200

    Space (part 3)

--- 4.JavaCollections/src/com/codegym/task/task25/task2515/Space.java
+++ 4.JavaCollections/src/com/codegym/task/task25/task2515/Space.java
@@ -1,7 +1,51 @@
 package com.codegym.task.task25.task2515;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Space {
 
+    private int width;
+    private int height;
+
+    private Spaceship ship;
+    private List<Ufo> ufos = new ArrayList<>();
+    private List<Rocket> rockets = new ArrayList<>();
+    private List<Bomb> bombs = new ArrayList<>();
+
+    public Space(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
     public static void main(String... args) {
+    }
+
+    public void setShip(Spaceship ship) {
+        this.ship = ship;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public Spaceship getShip() {
+        return ship;
+    }
+
+    public List<Ufo> getUfos() {
+        return ufos;
+    }
+
+    public List<Rocket> getRockets() {
+        return rockets;
+    }
+
+    public List<Bomb> getBombs() {
+        return bombs;
     }
 }
