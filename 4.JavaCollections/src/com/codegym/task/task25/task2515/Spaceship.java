e55a7ecd866063f5491fd6a2b9c5956eb537c9ac
Korwin <korwin-bieniek@wp.pl>
2021-06-05 07:06:35 +0200

    Space (part 5)

--- 4.JavaCollections/src/com/codegym/task/task25/task2515/Spaceship.java
+++ 4.JavaCollections/src/com/codegym/task/task25/task2515/Spaceship.java
@@ -1,4 +1,8 @@
 package com.codegym.task.task25.task2515;
 
 public class Spaceship extends BaseObject {
+
+    public Spaceship(double x, double y, double radius) {
+        super(x, y, radius);
+    }
 }
