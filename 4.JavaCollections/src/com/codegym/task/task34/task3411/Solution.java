dbadef101dcc54b2c53c3496cda370d221c86c89
Korwin <korwin-bieniek@wp.pl>
2021-06-06 05:33:22 +0200

    Tower of Hanoi

---
+++ 4.JavaCollections/src/com/codegym/task/task34/task3411/Solution.java
@@ -0,0 +1,23 @@
+package com.codegym.task.task34.task3411;
+
+/* 
+Tower of Hanoi
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int numRings = 3;
+        moveRing('A', 'B', 'C', numRings);
+    }
+
+    public static void moveRing(char a, char b, char c, int numRings) {
+        if (numRings > 1) {
+            moveRing(a, c, b, numRings - 1);
+            System.out.println("from " + a + " to " + b);
+            moveRing(c, b, a, numRings - 1);
+        } else {
+            System.out.println("from " + a + " to " + b);
+        }
+    }
+}
