abe70eaf850957045c035df8d59f7b8ab8556350
Korwin <korwin-bieniek@wp.pl>
2021-06-06 05:31:02 +0200

    Fibonacci numbers using recursion

---
+++ 4.JavaCollections/src/com/codegym/task/task34/task3401/Solution.java
@@ -0,0 +1,23 @@
+package com.codegym.task.task34.task3401;
+
+/* 
+Fibonacci numbers using recursion
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        System.out.println(solution.fibonacci(9));     //34
+        System.out.println(solution.fibonacci(5));     //5
+        System.out.println(solution.fibonacci(2));     //1
+        System.out.println(solution.fibonacci(1));     //1
+    }
+
+    public int fibonacci(int n) {
+        if (n == 1 || n == 2)
+            return 1;
+        else return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
