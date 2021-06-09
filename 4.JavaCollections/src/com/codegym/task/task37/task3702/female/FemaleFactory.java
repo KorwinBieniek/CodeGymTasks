bd214b9b694925c0f7ea53461743a1535046099f
Korwin <korwin-bieniek@wp.pl>
2021-06-09 20:14:29 +0200

    Factories (part 3)

---
+++ 4.JavaCollections/src/com/codegym/task/task37/task3702/female/FemaleFactory.java
@@ -0,0 +1,18 @@
+package com.codegym.task.task37.task3702.female;
+
+import com.codegym.task.task37.task3702.Human;
+
+public class FemaleFactory {
+
+    public Human getPerson(int age) {
+        Human human = null;
+        if (age <= KidGirl.MAX_AGE) {
+            human = new KidGirl();
+        } else
+            if (age <= TeenGirl.MAX_AGE) {
+                human = new TeenGirl();
+            } else
+                human = new Woman();
+        return human;
+    }
+}
