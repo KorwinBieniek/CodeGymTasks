b7231f59b50a38aacbd1e31eb658277e1517d76f
Korwin <korwin-bieniek@wp.pl>
2021-06-09 20:26:11 +0200

    Find the class by its description

---
+++ 4.JavaCollections/src/com/codegym/task/task36/task3607/Solution.java
@@ -0,0 +1,18 @@
+package com.codegym.task.task36.task3607;
+
+import java.util.concurrent.DelayQueue;
+
+/* 
+Find the class by its description
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(getExpectedClass());
+    }
+
+    public static Class getExpectedClass() {
+        return DelayQueue.class;
+    }
+}
