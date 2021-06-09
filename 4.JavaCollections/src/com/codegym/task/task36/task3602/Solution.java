873a4778bb7a73971bd472408c259a4e577e5782
Korwin <korwin-bieniek@wp.pl>
2021-06-09 20:20:30 +0200

    Find the class by its description

---
+++ 4.JavaCollections/src/com/codegym/task/task36/task3602/Solution.java
@@ -0,0 +1,23 @@
+package com.codegym.task.task36.task3602;
+
+import java.util.Collections;
+
+/* 
+Find the class by its description
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(getExpectedClass());
+    }
+
+    public static Class getExpectedClass() {
+        for (Class clazz : Collections.class.getDeclaredClasses()) {
+            if (clazz.getSimpleName().equals("EmptyList")) {
+                return clazz;
+            }
+        }
+        return null;
+    }
+}
