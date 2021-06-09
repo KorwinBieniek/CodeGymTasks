5c4bb53a5b51414ea112e391f040d238eaa06ee4
Korwin <korwin-bieniek@wp.pl>
2021-06-09 09:31:37 +0200

    Introducing generics

---
+++ 4.JavaCollections/src/com/codegym/task/task35/task3502/Solution.java
@@ -0,0 +1,17 @@
+package com.codegym.task.task35.task3502;
+
+import java.util.List;
+
+/* 
+Introducing generics
+
+*/
+
+public class Solution<T extends List<Solution.SomeClass>> {
+    public static class SomeClass<K extends Number> {
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
