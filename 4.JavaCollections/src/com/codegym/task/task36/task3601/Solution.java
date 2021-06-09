b8cd9347dd89f4c6db015c9f6718290c10630d11
Korwin <korwin-bieniek@wp.pl>
2021-06-09 20:18:48 +0200

    MVC - a simplified version

---
+++ 4.JavaCollections/src/com/codegym/task/task36/task3601/Solution.java
@@ -0,0 +1,12 @@
+package com.codegym.task.task36.task3601;
+
+/* 
+MVC - a simplified version
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        new View().fireShowDataEvent();
+    }
+}
