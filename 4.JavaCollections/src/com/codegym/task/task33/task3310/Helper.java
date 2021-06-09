a8ff334d41ecf387bd3051ffa7bcfbfc1e19d50a
Korwin <korwin-bieniek@wp.pl>
2021-06-09 20:41:55 +0200

    Shortener (part 5)

--- 4.JavaCollections/src/com/codegym/task/task33/task3310/Helper.java
+++ 4.JavaCollections/src/com/codegym/task/task33/task3310/Helper.java
@@ -6,7 +6,7 @@ import java.security.SecureRandom;
 public class Helper {
     public static String generateRandomString() {
         SecureRandom random = new SecureRandom();
-        return new BigInteger(130, random).toString(36);
+        return new BigInteger(130, random).toString(32);
     }
 
     public static void printMessage(String message) {
