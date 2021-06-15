4287c446f5829748a9479e56ad1126f74b8ad99a
Korwin <korwin-bieniek@wp.pl>
2021-06-15 06:23:06 +0200

    Template method

---
+++ 4.JavaCollections/src/com/codegym/task/task37/task3712/Game.java
@@ -0,0 +1,13 @@
+package com.codegym.task.task37.task3712;
+
+public abstract class Game {
+    public abstract void prepareForTheGame();
+    public abstract void playGame();
+    public abstract void congratulateWinner();
+
+    public void run() {
+        prepareForTheGame();
+        playGame();
+        congratulateWinner();
+    }
+}
