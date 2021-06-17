1fa655bb1fa806f6dee6713e0023f266843a1a2e
Korwin <korwin-bieniek@wp.pl>
2021-06-17 10:03:29 +0200

    Checked exceptions

---
+++ 4.JavaCollections/src/com/codegym/task/task38/task3802/VeryComplexClass.java
@@ -0,0 +1,16 @@
+package com.codegym.task.task38.task3802;
+
+/* 
+Checked exceptions
+
+*/
+
+public class VeryComplexClass {
+    public void veryComplexMethod() throws Exception {
+        this.clone();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
