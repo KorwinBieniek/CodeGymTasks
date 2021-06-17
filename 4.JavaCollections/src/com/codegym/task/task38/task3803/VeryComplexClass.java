658ef0a26e438aba9d2281e353ba9d8bd5af5f4c
Korwin <korwin-bieniek@wp.pl>
2021-06-17 10:04:04 +0200

    Runtime exceptions (unchecked exceptions)

---
+++ 4.JavaCollections/src/com/codegym/task/task38/task3803/VeryComplexClass.java
@@ -0,0 +1,26 @@
+package com.codegym.task.task38.task3803;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/* 
+Runtime exceptions (unchecked exceptions)
+
+*/
+
+public class VeryComplexClass {
+    public void methodThrowsClassCastException() {
+        List<Integer> list = new ArrayList<Integer>();
+        ((LinkedList<Integer>) list).add(3);
+    }
+
+    public void methodThrowsNullPointerException() {
+        int [] arr = null;
+        int i = arr.length;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
