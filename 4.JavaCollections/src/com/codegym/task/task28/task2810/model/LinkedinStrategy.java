a8bb023fcbd7d44bae4690dcf508cccf05017181
Korwin <korwin-bieniek@wp.pl>
2021-06-18 20:36:20 +0200

    Aggregator (part 4)

--- 4.JavaCollections/src/com/codegym/task/task28/task2810/model/LinkedinStrategy.java
+++ 4.JavaCollections/src/com/codegym/task/task28/task2810/model/LinkedinStrategy.java
@@ -1,4 +1,6 @@
 package com.codegym.task.task28.task2810.model;
 
 public class LinkedinStrategy implements Strategy {
+
+    private static final String URL_FORMAT = "https://www.linkedin.com/jobs/search?keywords=java+%s&start=%d";
 }
