0067d50a03e64030ab94068f9b56c06c1f488eee
Korwin <korwin-bieniek@wp.pl>
2021-06-18 20:50:52 +0200

    Aggregator (part 15)

--- 4.JavaCollections/src/com/codegym/task/task28/task2810/view/HtmlView.java
+++ 4.JavaCollections/src/com/codegym/task/task28/task2810/view/HtmlView.java
@@ -2,6 +2,10 @@ package com.codegym.task.task28.task2810.view;
 
 import com.codegym.task.task28.task2810.Controller;
 import com.codegym.task.task28.task2810.vo.JobPosting;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -34,7 +38,43 @@ public class HtmlView implements View {
     }
 
     private String getUpdatedFileContents(List<JobPosting> vacancies) {
-        return null;
+        try {
+            Document doc = getDocument();
+            Elements templateHidden = doc.getElementsByClass("template");
+            Element template = templateHidden.clone().removeAttr("style").removeClass("template").get(0);
+
+            //remove all prev vacancies
+            Elements prevVacancies = doc.getElementsByClass("vacancy");
+
+            for (Element redundant : prevVacancies) {
+                if (!redundant.hasClass("template")) {
+                    redundant.remove();
+                }
+            }
+
+            //add new vacancies
+            for (JobPosting vacancy : vacancies) {
+                Element vacancyElement = template.clone();
+
+                Element vacancyLink = vacancyElement.getElementsByAttribute("href").get(0);
+                vacancyLink.appendText(vacancy.getTitle());
+                vacancyLink.attr("href", vacancy.getUrl());
+                Element city = vacancyElement.getElementsByClass("city").get(0);
+                city.appendText(vacancy.getCity());
+                Element companyName = vacancyElement.getElementsByClass("companyName").get(0);
+                companyName.appendText(vacancy.getCompanyName());
+
+                templateHidden.before(vacancyElement.outerHtml());
+            }
+            return doc.html();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "Some exception occurred";
+    }
+
+    protected Document getDocument() throws IOException {
+        return Jsoup.parse(new File(filePath), "UTF-8");
     }
 
     private void updateFile(String content) {
