a0b8f6d851236fc71f54a4bd27d322887788ac10
Korwin <korwin-bieniek@wp.pl>
2021-06-18 20:32:59 +0200

    Aggregator (part 2)

--- 4.JavaCollections/src/com/codegym/task/task28/task2810/Aggregator.java
+++ 4.JavaCollections/src/com/codegym/task/task28/task2810/Aggregator.java
@@ -1,7 +1,12 @@
 package com.codegym.task.task28.task2810;
 
+import com.codegym.task.task28.task2810.model.Provider;
+
 public class Aggregator {
 
     public static void main(String[] args) {
+        Provider provider = new Provider(null);
+        Controller controller = new Controller(provider);
+        System.out.println(controller);
     }
 }
