4dd0dfe117b5e7cdf6c3ce197a2c3359ac4d4d3d
Korwin <korwin-bieniek@wp.pl>
2021-06-18 20:38:08 +0200

    Aggregator (part 7)

--- 4.JavaCollections/src/com/codegym/task/task28/task2810/model/LinkedinStrategy.java
+++ 4.JavaCollections/src/com/codegym/task/task28/task2810/model/LinkedinStrategy.java
@@ -10,12 +10,15 @@ import java.util.List;
 
 public class LinkedinStrategy implements Strategy {
 
-    private static final String URL_FORMAT = "https://www.linkedin.com/jobs/search?keywords=Java+%s&start=%d";
+    private static final String URL_FORMAT = "https://www.linkedin.com/jobs/search?keywords=java+%s&start=%d";
 
     @Override
     public List<JobPosting> getJobPostings(String searchString) {
         try {
-            Document document = Jsoup.connect(String.format(URL_FORMAT, "MyTown", 0)).userAgent("Mozilla/5.0 (jsoup)").get();
+            Document document = Jsoup.connect(String.format(URL_FORMAT, "MyTown", 0))
+                    .userAgent("Mozilla/5.0 (jsoup)")
+                    .referrer("https://www.linkedin.com/")
+                    .get();
         } catch (IOException e) {
             e.printStackTrace();
         }
