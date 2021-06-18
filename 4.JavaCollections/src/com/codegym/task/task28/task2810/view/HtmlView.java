4a232f9361be432889d671f76f89daab028c1b0d
Korwin <korwin-bieniek@wp.pl>
2021-06-18 20:50:12 +0200

    Aggregator (part 14)

--- 4.JavaCollections/src/com/codegym/task/task28/task2810/view/HtmlView.java
+++ 4.JavaCollections/src/com/codegym/task/task28/task2810/view/HtmlView.java
@@ -3,6 +3,9 @@ package com.codegym.task.task28.task2810.view;
 import com.codegym.task.task28.task2810.Controller;
 import com.codegym.task.task28.task2810.vo.JobPosting;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 
 public class HtmlView implements View {
@@ -35,6 +38,11 @@ public class HtmlView implements View {
     }
 
     private void updateFile(String content) {
-
+        File file = new File(filePath);
+        try (FileOutputStream outputStream = new FileOutputStream(file)) {
+            outputStream.write(content.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
