5fd9d61c54e7604d11ab2c9c5fc4181256b6505c
Korwin <korwin-bieniek@wp.pl>
2021-06-18 20:37:36 +0200

    Aggregator (part 6)

--- 4.JavaCollections/src/com/codegym/task/task28/task2810/model/LinkedinStrategy.java
+++ 4.JavaCollections/src/com/codegym/task/task28/task2810/model/LinkedinStrategy.java
@@ -1,16 +1,24 @@
 package com.codegym.task.task28.task2810.model;
 
 import com.codegym.task.task28.task2810.vo.JobPosting;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class LinkedinStrategy implements Strategy {
 
-    private static final String URL_FORMAT = "https://www.linkedin.com/jobs/search?keywords=java+%s&start=%d";
+    private static final String URL_FORMAT = "https://www.linkedin.com/jobs/search?keywords=Java+%s&start=%d";
 
     @Override
     public List<JobPosting> getJobPostings(String searchString) {
+        try {
+            Document document = Jsoup.connect(String.format(URL_FORMAT, "MyTown", 0)).userAgent("Mozilla/5.0 (jsoup)").get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return new ArrayList<>();
     }
 }
--- 4.JavaCollections/src/com/codegym/task/task28/task2810/vo/JobPosting.java
+++ 4.JavaCollections/src/com/codegym/task/task28/task2810/vo/JobPosting.java
@@ -8,6 +8,31 @@ public class JobPosting {
     private String websiteName;
     private String url;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        JobPosting vacancy = (JobPosting) o;
+
+        if (title != null ? !title.equals(vacancy.title) : vacancy.title != null) return false;
+        if (city != null ? !city.equals(vacancy.city) : vacancy.city != null) return false;
+        if (companyName != null ? !companyName.equals(vacancy.companyName) : vacancy.companyName != null) return false;
+        if (websiteName != null ? !websiteName.equals(vacancy.websiteName) : vacancy.websiteName != null) return false;
+        return url != null ? url.equals(vacancy.url) : vacancy.url == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = title != null ? title.hashCode() : 0;
+        result = 31 * result + (city != null ? city.hashCode() : 0);
+        result = 31 * result + (companyName != null ? companyName.hashCode() : 0);
+        result = 31 * result + (websiteName != null ? websiteName.hashCode() : 0);
+        result = 31 * result + (url != null ? url.hashCode() : 0);
+        return result;
+    }
+
     public String getTitle() {
         return title;
     }
