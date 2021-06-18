b74140c116eda494657ce667f477c7d807411c6e
Korwin <korwin-bieniek@wp.pl>
2021-06-18 07:04:03 +0200

    Tickets

---
+++ 4.JavaCollections/src/com/codegym/task/task38/task3811/Solution.java
@@ -0,0 +1,18 @@
+package com.codegym.task.task38.task3811;
+
+@Ticket (
+        priority = Ticket.Priority.HIGH,
+        createdBy = "Noodles",
+        tags = {"bug","asap" }
+)
+
+/* 
+Tickets
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+}
