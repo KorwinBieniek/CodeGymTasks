155dd9195ecef81298d4b0b63cd2ef82ba4f46b3
Korwin <korwin-bieniek@wp.pl>
2021-06-18 20:31:42 +0200

    Aggregator (part 1)

---
+++ 4.JavaCollections/src/com/codegym/task/task28/task2810/Aggregator.java
@@ -0,0 +1,7 @@
+package com.codegym.task.task28.task2810;
+
+public class Aggregator {
+
+    public static void main(String[] args) {
+    }
+}
---
+++ 4.JavaCollections/src/com/codegym/task/task28/task2810/model/Strategy.java
@@ -0,0 +1,4 @@
+package com.codegym.task.task28.task2810.model;
+
+public interface Strategy {
+}
