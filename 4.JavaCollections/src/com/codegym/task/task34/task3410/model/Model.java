f86db49c9e9319b70908e7748d68c9149bdf3adc
Korwin <korwin-bieniek@wp.pl>
2021-06-19 08:23:17 +0200

    Sokoban (part 9)

--- 4.JavaCollections/src/com/codegym/task/task34/task3410/model/Model.java
+++ 4.JavaCollections/src/com/codegym/task/task34/task3410/model/Model.java
@@ -1,5 +1,13 @@
 package com.codegym.task.task34.task3410.model;
 
+import com.codegym.task.task34.task3410.controller.EventListener;
+
 public class Model {
     public static final int BOARD_CELL_SIZE = 20;
+
+    private EventListener eventListener;
+
+    public void setEventListener(EventListener eventListener) {
+        this.eventListener = eventListener;
+    }
 }
--- 4.JavaCollections/src/com/codegym/task/task34/task3410/view/Board.java
+++ 4.JavaCollections/src/com/codegym/task/task34/task3410/view/Board.java
@@ -1,13 +1,20 @@
 package com.codegym.task.task34.task3410.view;
 
+import com.codegym.task.task34.task3410.controller.EventListener;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class Board extends JPanel {
     View view;
+    private EventListener eventListener;
 
     public Board(View view) {
         this.view = view;
+    }
+
+    public void setEventListener(EventListener eventListener) {
+        this.eventListener = eventListener;
     }
 
     @Override
