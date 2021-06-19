e38e7155da789e89b3cdf1f5866df04f9b15eb01
Korwin <korwin-bieniek@wp.pl>
2021-06-19 08:17:25 +0200

    Sokoban (part 3)

---
+++ 4.JavaCollections/src/com/codegym/task/task34/task3410/view/Board.java
@@ -0,0 +1,16 @@
+package com.codegym.task.task34.task3410.view;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Board extends JPanel {
+    View view;
+
+    public Board(View view) {
+        this.view = view;
+    }
+
+    @Override
+    public void paint(Graphics g) {
+    }
+}
--- 4.JavaCollections/src/com/codegym/task/task34/task3410/view/View.java
+++ 4.JavaCollections/src/com/codegym/task/task34/task3410/view/View.java
@@ -6,8 +6,20 @@ import javax.swing.*;
 
 public class View extends JFrame {
     private Controller controller;
+    private Board board;
 
     public View(Controller controller) {
         this.controller = controller;
+    }
+
+    public void init() {
+        board = new Board(this);
+        add(board);
+
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(500, 500);
+        setLocationRelativeTo(null);
+        setTitle("Sokoban");
+        setVisible(true);
     }
 }
