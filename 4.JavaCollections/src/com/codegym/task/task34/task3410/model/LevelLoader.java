3a74a3de6a49be4efc581d60c7e36e0bc96fac10
Korwin <korwin-bieniek@wp.pl>
2021-06-19 08:24:35 +0200

    Sokoban (part 10)

---
+++ 4.JavaCollections/src/com/codegym/task/task34/task3410/model/LevelLoader.java
@@ -0,0 +1,32 @@
+package com.codegym.task.task34.task3410.model;
+
+import java.nio.file.Path;
+import java.util.HashSet;
+import java.util.Set;
+
+public class LevelLoader {
+    public LevelLoader(Path levels) {
+    }
+
+    public GameObjects getLevel(int level) {
+
+
+        Set<Wall> walls = new HashSet<>();
+        Set<Box> boxes = new HashSet<>();
+        Set<StorageLocation> storageLocations = new HashSet<>();
+        Player player;
+
+        int x = Model.BOARD_CELL_SIZE / 2;
+        int y = Model.BOARD_CELL_SIZE / 2;
+
+        walls.add(new Wall(x + 0 * Model.BOARD_CELL_SIZE, y));
+        walls.add(new Wall(x + 1 * Model.BOARD_CELL_SIZE, y));
+        walls.add(new Wall(x + 2 * Model.BOARD_CELL_SIZE, y));
+
+        boxes.add(new Box(x, y + 0 * Model.BOARD_CELL_SIZE));
+        storageLocations.add(new StorageLocation(x, y + 1 * Model.BOARD_CELL_SIZE));
+        player = new Player(x, y + 2 * Model.BOARD_CELL_SIZE);
+
+        return new GameObjects(walls, boxes, storageLocations, player);
+    }
+}
