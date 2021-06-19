b3508af920b391ce7f7e43162cd1b5410275d6c3
Korwin <korwin-bieniek@wp.pl>
2021-06-19 08:20:45 +0200

    Sokoban (part 6)

---
+++ 4.JavaCollections/src/com/codegym/task/task34/task3410/model/StorageLocation.java
@@ -0,0 +1,24 @@
+package com.codegym.task.task34.task3410.model;
+
+
+import java.awt.*;
+
+public class StorageLocation extends GameObject {
+    private static final int SIZE = 2;
+
+    public StorageLocation(int x, int y) {
+        super(x, y, SIZE, SIZE);
+    }
+
+    @Override
+    public void draw(Graphics graphics) {
+        graphics.setColor(Color.RED);
+
+        int xc = getX();
+        int yc = getY();
+        int height = getHeight();
+        int width = getWidth();
+
+        graphics.drawOval(xc - width/2, yc - height/2, width, height);
+    }
+}
