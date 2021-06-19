bd88c160bafc0ae11f95ea3a7607430594be9e8a
Korwin <korwin-bieniek@wp.pl>
2021-06-19 06:38:40 +0200

    CashMachine (part 8)

--- 4.JavaCollections/src/com/codegym/task/task26/task2613/command/DepositCommand.java
+++ 4.JavaCollections/src/com/codegym/task/task26/task2613/command/DepositCommand.java
@@ -1,8 +1,30 @@
 package com.codegym.task.task26.task2613.command;
 
+import com.codegym.task.task26.task2613.ConsoleHelper;
+import com.codegym.task.task26.task2613.CurrencyManipulator;
+import com.codegym.task.task26.task2613.CurrencyManipulatorFactory;
+
+
 class DepositCommand implements Command {
 
+
     @Override
-    public void execute() {
+    public void execute()  {
+        ConsoleHelper.writeMessage("Making deposit...");
+        String currencyCode = ConsoleHelper.requestCurrencyCode();
+        CurrencyManipulator manipulator = CurrencyManipulatorFactory.getManipulatorByCurrencyCode(currencyCode);
+
+        while (true) {
+            String[] split = ConsoleHelper.getTwoValidNumbers(currencyCode);
+            try {
+                int denomination = Integer.parseInt(split[0]);
+                int count = Integer.parseInt(split[1]);
+                manipulator.addAmount(denomination, count);
+                ConsoleHelper.writeMessage(String.format("%d %s was deposited successfully.", (denomination * count), currencyCode));
+                break;
+            } catch (NumberFormatException e) {
+                ConsoleHelper.writeMessage("Please enter valid data.");
+            }
+        }
     }
 }
--- 4.JavaCollections/src/com/codegym/task/task26/task2613/command/InfoCommand.java
+++ 4.JavaCollections/src/com/codegym/task/task26/task2613/command/InfoCommand.java
@@ -1,8 +1,24 @@
 package com.codegym.task.task26.task2613.command;
 
+import com.codegym.task.task26.task2613.ConsoleHelper;
+import com.codegym.task.task26.task2613.CurrencyManipulator;
+import com.codegym.task.task26.task2613.CurrencyManipulatorFactory;
+
 class InfoCommand implements Command {
 
     @Override
     public void execute() {
+        ConsoleHelper.writeMessage("Information:");
+        boolean hasMoney = false;
+        for (CurrencyManipulator manipulator : CurrencyManipulatorFactory.getAllCurrencyManipulators()) {
+            if (manipulator.hasMoney()) {
+                hasMoney = true;
+                ConsoleHelper.writeMessage("\t" + manipulator.getCurrencyCode() + " - " + manipulator.getTotalAmount());
+            }
+        }
+
+        if (!hasMoney) {
+            ConsoleHelper.writeMessage("No money available.");
+        }
     }
 }
